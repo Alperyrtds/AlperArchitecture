4aa79bb5b16e0edbd9ee67463a7b05e2adf0789c
alperyurtdas <alper.yurtdas@argedor.com>
2024-04-09 09:29:45 +0300

    Add project files.

---
+++ Common/Utils/AlperResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Enums;
+
+namespace Common.Utils;
+
+public class ApiResponse
+{
+    public string status { get; set; }
+    public string errorMessage { get; set; }
+    public object data { get; set; }
+    public ApiResponse()
+    {
+
+    }
+    public ApiResponse(string _status, string _errorMessage, object _data)
+    {
+        status = _status;
+        errorMessage = _errorMessage;
+        data = _data;
+    }
+}
---
+++ WebApi/Controllers/KullaniciController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KullaniciController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public KullaniciController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        
+    }
+}
